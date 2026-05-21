c24438db7e68c39d18c603f2f66878774b2f40c6
CHANDAWAR SAMIKSHA SANJAY <samikshachandawar@gmail.com>
2026-05-21 19:46:14 +0530

    Create main.py

---
+++ Day-01-Variables-and-Data-Types/main.py
@@ -0,0 +1,17 @@
+# Variables and Data Types
+
+name = "Sam"
+age = 20
+height = 5.7
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
+
+# Type Checking
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
