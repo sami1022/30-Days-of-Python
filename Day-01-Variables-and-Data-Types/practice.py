171411e3beb471bf387f26ce3def245a72bc2efa
CHANDAWAR SAMIKSHA SANJAY <samikshachandawar@gmail.com>
2026-05-21 19:47:45 +0530

    Create practice.py

---
+++ Day-01-Variables-and-Data-Types/practice.py
@@ -0,0 +1,36 @@
+# Practice Questions
+
+# 1. Create variables
+college = "ABC College"
+percentage = 89.5
+city = "Pune"
+
+# 2. Print variables
+print("College:", college)
+print("Percentage:", percentage)
+print("City:", city)
+
+# 3. Check data types
+print(type(college))
+print(type(percentage))
+print(type(city))
+
+# Mini Task
+name = "Sam"
+course = "Computer Engineering"
+
+print("Name:", name)
+print("Course:", course)
+
+
+Output:
+College: ABC College
+Percentage: 89.5
+City: Pune
+<class 'str'>
+<class 'float'>
+<class 'str'>
+Name: Sam
+Course: Computer Engineering
+
+=== Code Execution Successful ===
