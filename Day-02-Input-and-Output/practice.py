5c64d1fe59e91c1f6e8263c92208d4c6c27e4eb9
CHANDAWAR SAMIKSHA SANJAY <samikshachandawar@gmail.com>
2026-05-22 17:49:19 +0530

    Create practice.py

---
+++ Day-02-Input-and-Output/practice.py
@@ -0,0 +1,21 @@
+# Practice Questions
+
+# 1. Take two numbers from user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# 2. Print addition
+print("Addition:", num1 + num2)
+
+# 3. Student Information
+student_name = input("Enter student name: ")
+course = input("Enter course name: ")
+
+print("\n--- Student Info ---")
+print("Name:", student_name)
+print("Course:", course)
+
+# Mini Task
+city = input("Enter your city: ")
+print("Welcome from", city)
+
