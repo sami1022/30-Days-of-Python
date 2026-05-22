929f8c6fb3dd7696c6f7481083d945884169527c
CHANDAWAR SAMIKSHA SANJAY <samikshachandawar@gmail.com>
2026-05-22 17:47:49 +0530

    Create main.py

---
+++ Day-02-Input-and-Output/main.py
@@ -0,0 +1,22 @@
+# Input and Output
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+height = float(input("Enter your height: "))
+
+print("\n--- Student Details ---")
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+
+Output:
+Enter your name: samiksha
+Enter your age: 22
+Enter your height: 5.8
+
+--- Student Details ---
+Name: samiksha
+Age: 22
+Height: 5.8
+
+=== Code Execution Successful ===
