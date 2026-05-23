2c7361b3bd3e0475eefc722e2f84157631fc02a0
CHANDAWAR SAMIKSHA SANJAY <samikshachandawar@gmail.com>
2026-05-23 20:59:47 +0530

    Create practice.py

---
+++ Day-03-Operators/practice.py
@@ -0,0 +1,19 @@
+# Practice Questions
+
+# 1. Take two numbers from user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Arithmetic Operations
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
+
+# Comparison
+print("Is first number greater?", num1 > num2)
+
+# Mini Task
+marks = int(input("Enter your marks: "))
+
+print("Passed:", marks >= 35)
