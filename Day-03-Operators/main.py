2d2ce5ba99dd7639b83416b2cb1eda40ae6597de
CHANDAWAR SAMIKSHA SANJAY <samikshachandawar@gmail.com>
2026-05-23 20:59:18 +0530

    Create main.py

---
+++ Day-03-Operators/main.py
@@ -0,0 +1,26 @@
+# Operators in Python
+
+a = 15
+b = 4
+
+# Arithmetic Operators
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Modulus:", a % b)
+print("Floor Division:", a // b)
+print("Power:", a ** b)
+
+# Comparison Operators
+print(a > b)
+print(a == b)
+print(a != b)
+
+# Logical Operators
+x = True
+y = False
+
+print(x and y)
+print(x or y)
+print(not x)
