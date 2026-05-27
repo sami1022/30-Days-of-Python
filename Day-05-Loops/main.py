f09a5f6c763376bb372bfe657a1000572cd0de4c
CHANDAWAR SAMIKSHA SANJAY <samikshachandawar@gmail.com>
2026-05-27 23:52:47 +0530

    Create main.py

---
+++ Day-05-Loops/main.py
@@ -0,0 +1,27 @@
+
+# for Loop Example
+
+for i in range(1, 6):
+    print("Number:", i)
+
+# while Loop Example
+
+count = 1
+
+while count <= 5:
+    print("Count:", count)
+    count += 1
+
+# break Statement
+
+for i in range(1, 10):
+    if i == 5:
+        break
+    print(i)
+
+# continue Statement
+
+for i in range(1, 6):
+    if i == 3:
+        continue
+    print(i)
