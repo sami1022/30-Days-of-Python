ff693cb39e1555852224bdc8c5379882e8c7464c
CHANDAWAR SAMIKSHA SANJAY <samikshachandawar@gmail.com>
2026-05-29 23:18:39 +0530

    Create main.py

---
+++ Day-06-Functions/main.py
@@ -0,0 +1,28 @@
+# Function without Parameters
+
+def welcome():
+    print("Welcome to Python")
+
+welcome()
+
+# Function with Parameters
+
+def greet(name):
+    print("Hello,", name)
+
+greet("Sam")
+
+# Function with Return Value
+
+def add(a, b):
+    return a + b
+
+result = add(10, 20)
+print("Sum:", result)
+
+# Function to Find Square
+
+def square(number):
+    return number * number
+
+print("Square:", square(5))
