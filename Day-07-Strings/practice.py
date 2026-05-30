2424d51c94ecef371e776ca9f97b47bf42591edd
CHANDAWAR SAMIKSHA SANJAY <samikshachandawar@gmail.com>
2026-05-30 23:35:34 +0530

    Create practice.py

---
+++ Day-07-Strings/practice.py
@@ -0,0 +1,33 @@
+# Practice Question 1
+
+text = "Programming"
+
+print(text[0])
+print(text[-1])
+
+# Practice Question 2
+
+name = input("Enter your name: ")
+
+print("Uppercase:", name.upper())
+print("Lowercase:", name.lower())
+
+# Practice Question 3
+
+sentence = "Python is awesome"
+
+print(sentence.replace("awesome", "powerful"))
+
+# Practice Question 4
+
+word = "Developer"
+
+print(word[0:4])
+
+# Mini Task
+
+full_name = input("Enter your full name: ")
+
+print("Name Length:", len(full_name))
+print("Uppercase:", full_name.upper())
+print("Lowercase:", full_name.lower())
