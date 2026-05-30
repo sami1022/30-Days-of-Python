e4856d2c2d44e12c719b13b73b51f357ffba5828
CHANDAWAR SAMIKSHA SANJAY <samikshachandawar@gmail.com>
2026-05-30 23:35:52 +0530

    Create main.py

---
+++ Day-07-Strings/main.py
@@ -0,0 +1,25 @@
+# Creating Strings
+
+name = "Python Programming"
+
+# Indexing
+print(name[0])
+print(name[7])
+
+# Slicing
+print(name[0:6])
+print(name[7:18])
+
+# String Methods
+print(name.upper())
+print(name.lower())
+print(name.replace("Python", "Java"))
+
+# Concatenation
+first_name = "Sam"
+last_name = "Smith"
+
+print(first_name + " " + last_name)
+
+# Length of String
+print("Length:", len(name))
