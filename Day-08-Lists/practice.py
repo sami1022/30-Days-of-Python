acbf5477cc977bea6dbd3c5198341a69d4b819d6
CHANDAWAR SAMIKSHA SANJAY <samikshachandawar@gmail.com>
2026-05-31 23:30:39 +0530

    Create practice.py

---
+++ Day-08-Lists/practice.py
@@ -0,0 +1,36 @@
+# Practice Question 1
+
+numbers = [10, 20, 30, 40, 50]
+
+print(numbers[0])
+print(numbers[-1])
+
+# Practice Question 2
+
+numbers.append(60)
+
+print(numbers)
+
+# Practice Question 3
+
+numbers.remove(30)
+
+print(numbers)
+
+# Practice Question 4
+
+for num in numbers:
+    print(num)
+
+# Mini Task
+
+students = ["Sam", "John", "Alex"]
+
+students.append("David")
+
+print("Students List:")
+
+for student in students:
+    print(student)
+
+print("Total Students:", len(students))
