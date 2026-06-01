b072ab561e119804e21826db08fe5f98ba5d969d
CHANDAWAR SAMIKSHA SANJAY <samikshachandawar@gmail.com>
2026-06-01 23:09:53 +0530

    Create practice.py

---
+++ Day-09-Tuples/practice.py
@@ -0,0 +1,29 @@
+# Practice Question 1
+
+colors = ("Red", "Green", "Blue")
+
+print(colors[0])
+print(colors[-1])
+
+# Practice Question 2
+
+numbers = (5, 10, 15, 20, 10)
+
+print("Count:", numbers.count(10))
+
+# Practice Question 3
+
+print("Index:", numbers.index(15))
+
+# Practice Question 4
+
+for number in numbers:
+    print(number)
+
+# Mini Task
+
+student = ("Sam", 20, "Python")
+
+print("Name:", student[0])
+print("Age:", student[1])
+print("Course:", student[2])
