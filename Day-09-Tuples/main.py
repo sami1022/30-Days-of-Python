4c58f93791506a4c73d4c4950bfa3f03e85d2f67
CHANDAWAR SAMIKSHA SANJAY <samikshachandawar@gmail.com>
2026-06-01 23:10:11 +0530

    Create main.py

---
+++ Day-09-Tuples/main.py
@@ -0,0 +1,26 @@
+# Creating a Tuple
+
+fruits = ("Apple", "Banana", "Mango")
+
+print("Tuple:", fruits)
+
+# Accessing Elements
+
+print("First Fruit:", fruits[0])
+print("Second Fruit:", fruits[1])
+
+# Tuple Slicing
+
+print("Sliced Tuple:", fruits[0:2])
+
+# Tuple Methods
+
+numbers = (10, 20, 30, 20, 40)
+
+print("Count of 20:", numbers.count(20))
+print("Index of 30:", numbers.index(30))
+
+# Loop Through Tuple
+
+for fruit in fruits:
+    print(fruit)
