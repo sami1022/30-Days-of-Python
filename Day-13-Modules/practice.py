6d1d1baf9f727fbc32c0b476b48cc1e2326976f4
CHANDAWAR SAMIKSHA SANJAY <samikshachandawar@gmail.com>
2026-06-05 23:33:16 +0530

    Create practice.py

---
+++ Day-13-Modules/practice.py
@@ -0,0 +1,30 @@
+# Practice Question 1
+
+import math
+
+print("Square Root of 81:", math.sqrt(81))
+
+# Practice Question 2
+
+print("Power:", math.pow(2, 5))
+
+# Practice Question 3
+
+import random
+
+print("Random Number:", random.randint(1, 10))
+
+# Practice Question 4
+
+from math import factorial
+
+print("Factorial of 6:", factorial(6))
+
+# Mini Task
+
+import datetime
+
+current_date = datetime.datetime.now()
+
+print("Current Date and Time:")
+print(current_date)
