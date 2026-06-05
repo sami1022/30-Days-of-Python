dbe966f3959195b34e7dae30a7eb018d2a43ba3a
CHANDAWAR SAMIKSHA SANJAY <samikshachandawar@gmail.com>
2026-06-05 23:33:35 +0530

    Create main.py

---
+++ Day-13-Modules/main.py
@@ -0,0 +1,17 @@
+# Using Built-in Modules
+
+import math
+import random
+
+print("Square Root:", math.sqrt(64))
+print("Value of Pi:", math.pi)
+
+# Random Number
+
+print("Random Number:", random.randint(1, 100))
+
+# Import Specific Function
+
+from math import factorial
+
+print("Factorial of 5:", factorial(5))
